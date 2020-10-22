f80b6de804be29be91e62a73b62d9ef5819454b4
Rikki Kendall <yrk3@duke.edu>
2020-10-22 01:47:01 -0400

    added popups for services

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,40 +1,56 @@
-import React, { Component } from "react";
-import { Button, Modal } from 'react-bootstrap';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
 
-export default function SimpleModal() {
-  const [show, setShow] = React.useState(false);
+const useStyles = makeStyles((theme) => ({
+  typography: {
+    padding: theme.spacing(2),
+  },
+}));
 
-  const handleClose = () => setShow(false);
-  const handleOpen = () => setShow(true);
+export default function SimpleModal(props) {
+  const data = props.data;
 
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
-    <div>
-      <Button type="button" onClick={handleOpen}>
-        Open Modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-    </div>
+        <div>
+                    {" "}
+        <i className={data.icon} onClick={handleClick}></i>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}> <h3> { props.data.name } </h3> </Typography>
+        <img alt="image1" src={props.data.img} />
+        <img alt="image2" src={props.data.img2} />
+        <img alt="image3" src={props.data.img3} />
+        <img alt="image4" src={props.data.img4} />
+      </Popover>
+      </div>
+      
   );
 }
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,38 +1,7 @@
 import React, { Component } from "react";
-import { Button, Modal } from 'react-bootstrap';
+import SimpleModal from './modal';
 
 export class Services extends Component {
-
-  Example() {
-    const [show, setShow] = React.useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-
   render() {
     return (
       <div id="services" className="text-center">
@@ -43,12 +12,15 @@ export class Services extends Component {
               サービス
             </h3>
           </div>
+         
           <div className="row">
             {this.props.data
               ? this.props.data.map((d, i) => (
                   <div  key={`${d.name}-${i}`} className="col-md-4">
                     {" "}
-                    <i className={d.icon}></i>
+                    {/* <i className={d.icon}></i> */}
+                    <SimpleModal data={d} icon={<i className={d.icon}></i>} />
+                    {" "}
                     <div className="service-desc">
                       <h3>{d.name}</h3>
                       <p>{d.text}</p>
